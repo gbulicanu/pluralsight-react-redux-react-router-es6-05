6c4ae0e78b13c8fbbbb65f3df69c657bee66a0f6
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-05-12 10:01:50 +0300

    Add 404 page support

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
 import SiteHeader from './common/SiteHeader';
+import PageNotFound from './PageNotFound';
 
 function App(){
     return (
         <div className="container-fluid">
             <SiteHeader />
-            <Route exact path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route component={PageNotFound} />
+            </Switch>
         </div>
     );
 }

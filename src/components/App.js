b6db6d98ff087ebeac974a5eaba8d902260a16a1
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-05-12 09:53:59 +0300

    Add site header

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
+import SiteHeader from './common/SiteHeader';
 
 function App(){
     return (
         <div className="container-fluid">
+            <SiteHeader />
             <Route exact path="/" component={HomePage} />
             <Route path="/about" component={AboutPage} />
         </div>
